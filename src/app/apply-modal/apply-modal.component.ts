cf32fbc18021f0f624f1dfbd9fb138676531dd20
PIC-ATL\eric.wild <eric.wild@picworld.com>
2024-08-19 09:32:05 -0400

    Added Privacy element in config to true. This shows a checkbox with text from below. If the user checks the box they can submit the form and will receive SMS and email messages from PIC. 1) Updated the Privacy notice to "By checking this box, you consent to receive text messages (SMS or MMS) from PIC Group, Inc. Message and data rates may apply. You can opt out at any time." 2) Added height to section element to give space for new Privacy element.

--- src/app/apply-modal/apply-modal.component.ts
+++ src/app/apply-modal/apply-modal.component.ts
@@ -1,22 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  NovoFormGroup,
-  FormUtils,
-  NovoModalRef,
-  NovoModalParams,
-  TextBoxControl,
-  FileControl,
-  PickerControl,
-  SelectControl,
-  NovoToastService,
-  CheckboxControl,
-  FieldInteractionApi,
-} from 'novo-elements';
+import { Router } from '@angular/router';
 import { TranslateService } from 'chomsky';
-import { SettingsService } from '../services/settings/settings.service';
+import {
+  CheckboxControl,
+  FieldInteractionApi, FileControl, FormUtils, NovoFormGroup, NovoModalParams, NovoModalRef, NovoToastService, PickerControl,
+  SelectControl, TextBoxControl
+} from 'novo-elements';
 import { AnalyticsService } from '../services/analytics/analytics.service';
 import { ApplyService } from '../services/apply/apply.service';
-import { Router } from '@angular/router';
+import { SettingsService } from '../services/settings/settings.service';
 
 @Component({
   selector: 'app-apply-modal',
@@ -58,6 +50,8 @@ export class ApplyModalComponent implements OnInit {
   public ngOnInit(): void {
     this.job = this.params['job'];
     this.source = this.params['source'];
+    console.log('Test...');
+    console.log('source: ' +  this.source);
     this.setupForm();
   }
   public setupForm(): void {
